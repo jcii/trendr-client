99488fa7354a2ab62182895d62bd5e21f0afca34
Alexander Murphy <amurphy345@gmail.com>
2016-09-01 05:45:37 -0600

    working with login page

--- public/animate.js
+++ public/animate.js
@@ -138,11 +138,11 @@ var mouseX = 0, mouseY = 0,
 			}
 
 			function render() {
-				var timer = Date.now() * 0.0001;
+				var timer = Date.now() * 0.0002;
 
 					camera.position.x = Math.cos( timer ) * 200;
 					camera.position.z = Math.sin( timer ) * 200;
-					camera.position.y = Math.cos( timer ) * -200;
+					camera.position.y = Math.sin( timer ) * 200;
 					camera.lookAt( scene.position );
 				// camera.position.x += ( mouseX - camera.position.x ) * .05;
 				// camera.position.y += ( - mouseY + 200 - camera.position.y ) * .05;
