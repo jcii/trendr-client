0f9a58be2caa41f9f5f87e866c350496a3e1e363
Alexander Murphy <amurphy345@gmail.com>
2016-09-01 21:38:00 -0600

    working on password validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,14 +8,37 @@ import { UserService } from '../services/user.service'
   styleUrls: ['register.component.css'],
   providers: [UserService]
 })
+
 export class RegisterComponent implements OnInit {
+  user = {
+    password: '',
+    passconf: '', 
+    username: ''
+  }
+  error = {
+    message: '',
+    isError: false,
+    display: "none"
+  }
+  isPasswordMismatch = false
+  isPasswordShort = false
 
   constructor( private userService:UserService ) { }
   ngOnInit() { }
-  userRegister(event, user, password, passconfirm) {
-    console.log("now registering");
-    
+  userRegister(event, user, password, passconfirm) {    
     event.preventDefault()
+    if(this.validatePassMatch(password, passconfirm)){
+      this.isPasswordMismatch = false
+      this.error.isError = false
+      console.log("password match");
+      
+    } else {
+      console.log("pas mismatch");
+      this.isPasswordMismatch = true
+      this.error.isError = true
+      this.error.message = "Password Mismatch"
+      
+    }
     this.userService.registerUser({ user, password})
       .subscribe(
         response => {
@@ -25,5 +48,20 @@ export class RegisterComponent implements OnInit {
           console.log(error);
         }
       )
+  }
+  validatePass(){
+    if(this.validatePassMatch(this.user.password, this.user.passconf)){
+      console.log(this.user.passconf);
+      
+      console.log("pass matching");
+    }
+    console.log("validate");
+    
+  }
+  validatePassMatch(pass: String, passconf: String) {
+    return pass === passconf
+  }
+  validatePassLength(pass: String) {
+    return pass.length >= 6
   }
 }
