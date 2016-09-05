ef4d19d2aa1aca1267617a10fe4e2356a472efd5
Alexander Murphy <amurphy345@gmail.com>
2016-09-05 12:40:41 -0600

    working on multi axis chart

---
+++ src/app/components/hichart/hichart.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { HichartComponent } from './hichart.component';
+
+describe('Component: Hichart', () => {
+  it('should create an instance', () => {
+    let component = new HichartComponent();
+    expect(component).toBeTruthy();
+  });
+});
