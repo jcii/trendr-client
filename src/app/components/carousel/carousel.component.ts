6b9631d35927b296c06846c6d1b39de1e54eac7f
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-05 19:33:40 -0600

    charts in carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CAROUSEL_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { BarChartComponent } from '../../bar-chart'
 
 @Component({
   moduleId: module.id,
   selector: 'app-carousel',
   templateUrl: 'carousel.component.html',
-  styleUrls: ['bootstrap.min.css', 'carousel.component.css']
+  styleUrls: ['bootstrap.min.css', 'carousel.component.css'],
+  directives: [BarChartComponent]
 })
 export class CarouselComponent {
 
