a08b5b0466d23ed8d2bb291529f18499d9a3581b
cole sobel <colesobel@coles-MacBook-Air.local>
2016-09-05 20:03:33 -0600

    bluring slide shifter upon click

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CAROUSEL_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 import { BarChartComponent } from '../../bar-chart'
+import { HichartComponent } from '../hichart'
 
 @Component({
   moduleId: module.id,
   selector: 'app-carousel',
   templateUrl: 'carousel.component.html',
   styleUrls: ['bootstrap.min.css', 'carousel.component.css'],
-  directives: [BarChartComponent]
+  directives: [BarChartComponent, HichartComponent]
 })
 export class CarouselComponent {
 
+blur(elem) {
+  document.getElementById(elem).blur()
+}
 
 }
