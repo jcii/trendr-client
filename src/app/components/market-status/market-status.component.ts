713be4038693ff3bc58a21904340cc1db345732b
Alexander Murphy <amurphy345@gmail.com>
2016-09-07 12:24:33 -0600

    working on market status component adding files

---
+++ src/app/components/market-status/market-status.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-market-status',
+  templateUrl: 'market-status.component.html',
+  styleUrls: ['market-status.component.css']
+})
+export class MarketStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
