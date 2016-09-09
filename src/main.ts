2154552e8bc5d79b0aaaf0ed0c3a91c3eecce535
Alexander Murphy <amurphy345@gmail.com>
2016-09-09 13:06:00 -0600

    working on local storage

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,17 @@ import { APP_ROUTES_PROVIDER } from './app/app.routes'
 import {HTTP_PROVIDERS} from '@angular/http';
 import { CHART_DIRECTIVES } from 'ng2-charts/ng2-charts'
 import { trendResolver } from './app/resolvers/trend.resolve'
-import { LocalStorageService } from 'angular2-localStorage/LocalStorageEmitter'
 
 if (environment.production) {
   enableProdMode();
 }
+
 
 bootstrap(AppComponent, [
     disableDeprecatedForms(), 
     provideForms(),
     APP_ROUTES_PROVIDER, 
     HTTP_PROVIDERS,
-    trendResolver,
-    LocalStorageService
+    trendResolver
     ]);
+
